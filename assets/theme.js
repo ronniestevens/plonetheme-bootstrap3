fdd55513e680118c5a2087825b96341b41ef5fa3
Ronnie Stevens <ronnie@zoo.nl>
2013-10-25 06:07:32 +0200

    added newsflash

--- assets/theme.js
+++ assets/theme.js
@@ -2,3 +2,15 @@
 $('#zoek').click(function() {
     $( '#searchbox' ).animate({width: 'toggle'});
 });
+
+$('.carousel').carousel()
+
+/* Flex Slider */
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        controlNav: true,
+        pauseOnHover: true,
+        slideshowSpeed: 15000
+    });
+});
